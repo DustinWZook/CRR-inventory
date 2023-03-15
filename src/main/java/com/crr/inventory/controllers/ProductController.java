0b0e3192cb768fd54aa21e14fda15505e675162f
DustinWZook <dustin.w.zook@gmail.com>
2023-03-15 12:31:11 -0500

    added functionality to add a product

--- src/main/java/com/crr/inventory/controllers/ProductController.java
+++ src/main/java/com/crr/inventory/controllers/ProductController.java
@@ -7,17 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
     private final ProductRepository productDao;
 
-    private final ProductCategoryController productCategoryDao;
+    private final ProductCategoryRepository productCategoryDao;
 
-    private final ProductLocationController productLocationDao;
+    private final ProductLocationRepository productLocationDao;
 
 @Autowired
-    public ProductController(ProductRepository productDao, @Lazy ProductCategoryController productCategoryDao,@Lazy ProductLocationController productLocationDao) {
+    public ProductController(ProductRepository productDao, ProductCategoryRepository productCategoryDao,ProductLocationRepository productLocationDao) {
         this.productDao = productDao;
         this.productCategoryDao = productCategoryDao;
         this.productLocationDao = productLocationDao;
@@ -27,4 +30,22 @@ public class ProductController {
     public String index(){
     return "index";
     }
+
+    @PostMapping("/createProduct")
+    public String createProduct(@ModelAttribute Product product,@RequestParam(name = "newProductName") String productName,@RequestParam(name = "newProductBrand") String productBrand,@RequestParam(name = "newProductDescription") String productDescription,@RequestParam(name = "newProductQuantity") int productQuantity,@RequestParam(name = "newProductWholesalePrice") double productWholesale,@RequestParam(name = "newProductStorePrice") double productStorePrice) {
+        // save the ad...
+        // redirect to to the index with all the ads
+        product.setProductName(productName);
+        product.setBrandName(productBrand);
+        product.setDescription(productDescription);
+        product.setQuantity(productQuantity);
+        product.setWholesalePrice(productWholesale);
+        product.setStorePrice(productStorePrice);
+
+
+        productDao.save(product);
+
+        return "redirect:/";
+    }
+
 }
