a67577e48dee559950f23204aeb6de58ff973dda
DustinWZook <dustin.w.zook@gmail.com>
2023-03-24 10:14:21 -0500

    added the export to excel methods

--- src/main/java/com/crr/inventory/controllers/ProductController.java
+++ src/main/java/com/crr/inventory/controllers/ProductController.java
@@ -2,10 +2,18 @@ package com.crr.inventory.controllers;
 
 import com.crr.inventory.bean.*;
 import com.crr.inventory.repositories.ProductRepository;
+import com.crr.inventory.services.ProductExcelExporter;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -16,6 +24,25 @@ public class ProductController {
     public ProductController(ProductRepository productDao) {
         this.productDao = productDao;
     }
+
+
+    @GetMapping("/products/export/excel")
+    public void exportToExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=users_" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        List<Product> listProducts = productDao.findAll();
+
+        ProductExcelExporter excelExporter = new ProductExcelExporter(listProducts);
+
+        excelExporter.export(response);
+    }
+
 
     @GetMapping("/")
     public String showProducts(Model model) {
---
+++ src/main/java/com/crr/inventory/services/ProductExcelExporter.java
@@ -0,0 +1,103 @@
+package com.crr.inventory.services;
+
+import com.crr.inventory.bean.Product;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.apache.poi.ss.util.CellUtil.createCell;
+
+public class ProductExcelExporter {
+    private XSSFWorkbook workbook;
+    private XSSFSheet sheet;
+    private List<Product> listProducts;
+
+    public ProductExcelExporter(List<Product> listProducts) {
+        this.listProducts = listProducts;
+        workbook = new XSSFWorkbook();
+    }
+
+    private void writeHeaderLine() {
+        sheet = workbook.createSheet("Users");
+
+        Row row = sheet.createRow(0);
+
+        CellStyle style = workbook.createCellStyle();
+        XSSFFont font = workbook.createFont();
+        font.setBold(true);
+        font.setFontHeight(16);
+        style.setFont(font);
+
+        createCell(row, 0, "Product ID", style);
+        createCell(row, 1, "Name", style);
+        createCell(row, 2, "Brand", style);
+        createCell(row, 3, "Description", style);
+        createCell(row, 4, "Category", style);
+        createCell(row, 5, "Location", style);
+        createCell(row, 6, "Quantity", style);
+        createCell(row, 7, "Wholesale Price", style);
+        createCell(row, 8, "Wholesale Total", style);
+        createCell(row, 9, "Store Price", style);
+        createCell(row, 10, "Store Price Total", style);
+    }
+
+    private void createCell(Row row, int columnCount, Object value, CellStyle style) {
+        sheet.autoSizeColumn(columnCount);
+        Cell cell = row.createCell(columnCount);
+        if (value instanceof Integer) {
+            cell.setCellValue((Integer) value);
+        } else if (value instanceof Boolean) {
+            cell.setCellValue((Boolean) value);
+        }else {
+            cell.setCellValue((String) value);
+        }
+        cell.setCellStyle(style);
+    }
+
+    private void writeDataLines() {
+        int rowCount = 1;
+
+        CellStyle style = workbook.createCellStyle();
+        XSSFFont font = workbook.createFont();
+        font.setFontHeight(14);
+        style.setFont(font);
+
+        for (Product user : listProducts) {
+            Row row = sheet.createRow(rowCount++);
+            int columnCount = 0;
+
+            createCell(row, columnCount++, user.getId(), style);
+            createCell(row, columnCount++, user.getProductName(), style);
+            createCell(row, columnCount++, user.getBrandName(), style);
+            createCell(row, columnCount++, user.getDescription(), style);
+            createCell(row, columnCount++, user.getProductCategory(), style);
+            createCell(row, columnCount++, user.getProductLocation(), style);
+            createCell(row, columnCount++, user.getQuantity(), style);
+            createCell(row, columnCount++, user.getWholesalePrice(), style);
+            createCell(row, columnCount++, user.getTotalWholesalePrice(), style);
+            createCell(row, columnCount++, user.getStorePrice(), style);
+            createCell(row, columnCount++, user.getTotalStorePrice(), style);
+        }
+    }
+
+    public void export(HttpServletResponse response) throws IOException {
+        writeHeaderLine();
+        writeDataLines();
+
+        ServletOutputStream outputStream = response.getOutputStream();
+        workbook.write(outputStream);
+        workbook.close();
+
+        outputStream.close();
+
+    }
+
+}
